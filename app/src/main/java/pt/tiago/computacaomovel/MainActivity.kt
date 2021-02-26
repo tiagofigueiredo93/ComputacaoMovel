0f401271dd25de17269a8767211891908a34bd82
tiagofigueiredo93 <tiago.figueiredo.bcl@gmail.com>
2021-02-26 19:09:09 +0000

    Initial upload of file projects

---
+++ app/src/main/java/pt/tiago/computacaomovel/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.tiago.computacaomovel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
